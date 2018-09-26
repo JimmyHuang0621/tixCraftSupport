af19b2f4c5ff3b4d2a7865b646c9052361759669
jimmy.huang <ake4e4@gmail.com>
2018-09-27 04:21:45 +0800

    add game page

---
+++ Script/gameScript.js
@@ -0,0 +1,23 @@
+chrome.storage.local.get({
+    ProgramAuto: false,
+    ProgramDate: '2018-06-01'
+}, items => {
+    if (items.ProgramAuto) {
+        let dstr = items.ProgramDate.replace(/-/g, "/")
+        let target = $(".normal td:contains('" + dstr + "')").first();
+        console.log("date 1: " + dstr);
+
+        if (target.length) {
+            console.log("date 2: " + target.text());
+            let link = target.parent().find("input:button").attr("data-href");
+            if (link) {
+                console.log("link: " + link);
+                window.location.href = link;
+            } else {
+                console.log("not found link!");
+            }
+        } else {
+            console.log("not found program!");
+        }
+    }
+});
