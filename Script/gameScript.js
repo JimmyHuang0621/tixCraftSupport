e2bab3ada7f712d583a1e5158fdff413caf0e365
jimmy.huang <ake4e4@gmail.com>
2018-09-30 11:48:16 +0800

    update gameScript

--- Script/gameScript.js
+++ Script/gameScript.js
@@ -1,23 +1,20 @@
 chrome.storage.local.get({
-    ProgramAuto: false,
     ProgramDate: '2018-12-31'
 }, items => {
-    if (items.ProgramAuto) {
-        let dstr = items.ProgramDate.replace(/-/g, "/")
-        let target = $(".normal td:contains('" + dstr + "')").first();
-        console.log("date 1: " + dstr);
+    let dstr = items.ProgramDate.replace(/-/g, "/")
+    let target = $(".normal td:contains('" + dstr + "')").first();
+    console.log("date 1: " + dstr);
 
-        if (target.length) {
-            console.log("date 2: " + target.text());
-            let link = target.parent().find("input:button").attr("data-href");
-            if (link) {
-                console.log("link: " + link);
-                window.location.href = link;
-            } else {
-                console.log("not found link!");
-            }
+    if (target.length) {
+        console.log("date 2: " + target.text());
+        let link = target.parent().find("input:button").attr("data-href");
+        if (link) {
+            console.log("link: " + link);
+            window.location.href = link;
         } else {
-            console.log("not found program!");
+            console.log("not found link!");
         }
+    } else {
+        console.log("not found program!");
     }
 });
